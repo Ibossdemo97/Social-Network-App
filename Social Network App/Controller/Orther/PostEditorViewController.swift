1f2e3d7bdc2805bc39475d90f39b7478d19b5191
Ibossdemo97 <110977997+Ibossdemo97@users.noreply.github.com>
2023-10-10 09:53:05 +0700

    Fist commit

---
+++ Social Network App/Controller/Orther/PostEditorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostEditorViewController.swift
+//  Social Network App
+//
+//  Created by Luyện Hà Luyện on 10/10/2023.
+//
+
+import UIKit
+
+class PostEditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
